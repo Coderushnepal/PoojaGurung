e60290b34e881dcd0700ea72fb56dbdc1af5bf51
po-gurungpuja <pogurungpuja@gmail.com>
2020-07-23 06:41:18 +0545

    third commit balls Falling

--- Javascript/JS_Assignment_5/script_11.js
+++ Javascript/JS_Assignment_5/script_11.js
@@ -1,5 +1,10 @@
 function Ball()
 {
+    var that = this;
+    this.isDeleted = false;
+    var ballons = [];
+
+
     this.ball = document.createElement("div");
     this.ball.style.borderRadius = "50%";
 
@@ -44,7 +49,17 @@ function Ball()
     var colors = ["red","violet","green","blue","yellow","pink","indigo"];
     
     //ball.setColors(colors[Math.random() * colors.length]);
-    this.ball.style.backgroundColor = colors[parseInt(Math.random() * colors.length)];    
+    this.ball.style.backgroundColor = colors[parseInt(Math.random() * colors.length)];   
+    
+    
+
+    this.ball.addEventListener("click", function () {
+       // this.style.backgroundColor = "green";
+        that.isDeleted = true;
+        document.body.removeChild(that.ball);
+           // clearInterval(interval);
+            
+      });
         
     };
 
@@ -53,22 +68,74 @@ function Ball()
         document.body.appendChild(this.ball);
         // console.log(this.ball.id);
       };
+
+
+    this.movement = function() {
+        // setTimeout(function() {
+           this.interval = setInterval(
+                function() {                   
+                    // return  function () {
+                        var newBallTopValue = that.ball.style.top;
+                       // console.log(that.ball.style.top);
+                        var nextTopValue = parseInt(newBallTopValue) + (Math.random() * 5) ; 
+                        that.ball.style.top = nextTopValue + "px";
+                    // };    
+                }, 
+            1000/60 );
+    // }, 3000);
+        }
+
+        // this.remove = function() {
+        //     document.body.removeChild(this.ball);
+        //     clearInterval(interval);
+        //     this.isDeleted = true;
+        //     };
+        
+            // this.removeBall = function() {
+            //     setInterval(function(){
+            //         var ball = this.ball;
+            //         ballons.map(function(ball){
+            //              //console.log(ball);
+            //             if (ball.getWH().H > 0) {
+            //                 document.body.removeChild(this.ball);
+            // clearInterval(interval);
+            // this.isDeleted = true;
+            //             }
+            //         });
+            //     });
+            // }
+
+        
 }
 
 for (var i = 1; i <= 200; i++) {
 
-    var ball = new Ball();
+    var ball = new Ball();    
+   
+   
 
-    
-
-    var X = Math.random() * window.innerWidth + "px";
+    var X = Math.random() * (window.innerWidth) + "px";
     var Y = 0 + "px";
     ball.setXY(X,Y);
 
     var W = (Math.random()* 80 + 50) + "px";
-    var H = (Math.random()* 80 + 50) + "px";
-    ball.setWH(W,H);
-
+   // var H = (Math.random()* 80 + 50) + "px";
+    ball.setWH(W,W);
+   
     ball.create();
     ball.render();
-  }
+
+    var timeout = Math.random() * 5 * 1000;
+    setTimeout(
+        (function() {
+            var currentBall = this.ball;
+            return function() {
+                currentBall.movement();
+            };
+        }) (),
+        timeout
+    );
+
+   //ball.removeBall();
+}
+
