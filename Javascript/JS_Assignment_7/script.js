28ecfa5ababde29e64255ef5234916bae7773bc3
po-gurungpuja <pogurungpuja@gmail.com>
2020-07-27 21:43:26 +0545

    hangman letterdash and show wrong letters

--- Javascript/JS_Assignment_7/script.js
+++ Javascript/JS_Assignment_7/script.js
@@ -12,22 +12,59 @@ box1.appendChild(title);
 
 var container = document.getElementById("box3");
 var box2 = document.getElementById("box2");
-box2.style.marginTop = "50px";
-container.appendChild(box2);
+
 
 var words = ["sunday","october","apple","gentleman","dope","eat","four"];
 var nameWords = words[Math.floor(Math.random() * words.length)];
-//console.log(nameWords);
+console.log(nameWords);
 
-for(var i=0; i<nameWords.length; i++)
+for(var i=0; i < nameWords.length; i++)
 {
-    var letterDash = document.createElement("div");
-    letterDash.style.borderBottom = "5px solid white";
-    letterDash.style.width = "30px";
-    letterDash.style.height = "50px";
-    letterDash.style.display = "inline-block";
-    letterDash.style.marginLeft = "5px";    
-    box2.appendChild(letterDash);
+    var letter = document.createElement("div");
+    letter.classList.add("box-letter");
+    letter.setAttribute("id","dashLetter")
+   // letterDash.innerHTML = nameWords[i];
+    
+    box2.appendChild(letter);
     container.appendChild(box2);
 }
+
+var word = document.querySelectorAll("#dashLetter");
+var wrong1 = document.getElementById("box4");
+var wrong2 = document.createElement("span");
+var wrong3 = document.createTextNode("Wrong");
+wrong2.appendChild(wrong3);
+wrong1.appendChild(wrong2);
+
+
+
+document.addEventListener("keypress", function(e) {
+    console.log(e) 
+    if(nameWords.includes(e.key))
+    {
+      for (var n = 0; n < nameWords.length; n++)
+        {
+            if(nameWords[n] === e.key)
+            {
+                // console.log(e.key + " " + "match");
+                word[n].innerHTML = e.key;
+            }      
+        }
+    } 
+    else
+    {
+       // console.log("no match");
+       var wrongLetter = document.createElement("p");
+       wrongLetter.innerHTML = e.key;
+       wrong2.appendChild(wrongLetter);
+        
+    }
+    
+
+})
+    
+   
+
+
+
 
